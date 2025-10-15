969c0486f204dfea91814cda0aca7f4f4a4a3f88
md-alif007 <mohammadalif043@gmail.com>
2025-10-16 01:24:47 +0600

    added load video

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,31 @@ const loadAllLessons = () => {
     .then((data) => displayLessons(data.data));
 };
 
+const loadWordByLevels = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayWordByLevels(data));
+};
+
 const displayLessons = (lessons) => {
-  console.log(lessons);
+  //   console.log(lessons);
 
   const levelContainer = document.getElementById("level-container");
   for (const lesson of lessons) {
     const leveldiv = document.createElement("div");
 
     leveldiv.innerHTML = `
-    <button class="btn btn-outline btn-info">Lesson - ${lesson.level_no}</button>
+    <button class="btn btn-outline btn-info"><i class="fa-solid fa-book"></i>Lesson - ${lesson.level_no}</button>
     `;
     levelContainer.append(leveldiv);
   }
 };
 
+const displayWordByLevels = (words) => {
+    console.log(words);
+};
+
 loadAllLessons();
+loadWordByLevels();
